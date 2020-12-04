e16814fcf4f6ac682efc501498237189c3d481a4
Nikita Entin <user@MacNikita.local>
2020-12-04 18:27:25 +0300

    made equal rectangles

---
+++ equalRects/EqualRects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EqualRects
+//
+//  Created by Nikita Entin on 04.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
