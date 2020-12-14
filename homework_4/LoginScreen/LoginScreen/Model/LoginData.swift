8128fff2efacf1ed3814129187db52dfed0e9637
Nikita Entin <user@MacNikita.local>
2020-12-15 00:45:04 +0300

    fixed logic

--- homework_4/LoginScreen/LoginScreen/Model/LoginData.swift
+++ homework_4/LoginScreen/LoginScreen/Model/LoginData.swift
@@ -7,10 +7,10 @@
 
 
 struct LoginData {
-    private(set) var login = "Login"
-    private(set) var password = "Password"
+    private(set) var login = "log"
+    private(set) var password = "pass"
     var fullName: String {
-        "\(login) aka Nikita Entin"
+        "\(login.localizedCapitalized) aka Nikita Entin"
     }
 }
 
